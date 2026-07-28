0c01575ac6d4e8b0c27891e377c6ea6ca242d5eb
mcsewoody <mcsewoody@gmail.com>
2026-07-28 07:09:09 -0700

    Export data.js from Supabase [skip ci]

--- js/data.js
+++ js/data.js
@@ -584,7 +584,7 @@ window.DB = {
       ],
       "price": 22.9,
       "rating": 4.7,
-      "reviews": 999,
+      "reviews": 63,
       "badge": null,
       "name": {
         "en": "VIEMAG Suction Long-Arm Mount",
